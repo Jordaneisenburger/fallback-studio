c547260ef21b64347b245751b7117ab50eb87ec7
Jordan <jordan@experius.nl>
2019-04-02 21:58:49 +0200

    [UPGRADE] added resolution to fix graphql dependency

--- magento-compatibility.js
+++
@@ -1,9 +0,0 @@
-/**
- * This file describes PWA Studio to Magento version compatabilities.
- */
-
-// PWA Studio version -> Magento version.
-module.exports = {
-    '>2.0.0': '2.3.1',
-    '2.0.0': '2.3.0'
-};
